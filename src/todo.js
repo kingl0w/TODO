288b22df2559cac19ed5d9b14823ec4a4f868e9b
kingl0w <tdytomsawyer@protonmail.com>
2023-10-30 18:29:49 -0700

    todo

--- src/todo.js
+++ src/todo.js
@@ -7,6 +7,13 @@ export default function loadTodo() {
             const item = document.createElement('li');
             item.classList.add('todo-item');
             item.textContent = input.value;
+            const xButton = document.createElement('button');
+            xButton.textContent = 'X';
+            xButton.classList.add('x-button');
+            xButton.addEventListener('click', function () {
+                item.classList.add('completed');
+            });
+            item.appendChild(xButton);
             list.appendChild(item);
             input.value = '';
         }
